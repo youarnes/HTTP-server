ae500aaafbccf7fb8f25cf70d3d24ff06d5bb31f
youarnes <younes.ouarhim@gmail.com>
2026-01-03 21:54:49 +0100

    first draft of the TCP server upon which will be build the HTTP server

---
+++ script.sh
@@ -0,0 +1,2 @@
+g++ -o server server.cpp -lws2_32
+./server
---
+++ server.cpp
@@ -0,0 +1,83 @@
+// socket programming is a way for two computers or processes to talk over the network
+
+#include <winsock2.h>
+#include <iostream>
+#define DEFAULT_BUFLEN 1024
+int main () {
+    // Initialize WSA variables
+    WSADATA wsaData;
+    int wserr;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+    wserr = WSAStartup(wVersionRequested, &wsaData);
+    int iResult; // var to store integer intermediate results 
+
+    // Check for initialization success
+    if (wserr != 0) {
+        std::cout << "The winsock dll not found" << std::endl;
+        return 1;
+    } else {
+        std::cout << "The Winsock dll found" << std::endl;
+        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if ( serverSocket == INVALID_SOCKET ) {
+        printf("The server socket definition failed with error %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    iResult = bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if ( iResult == SOCKET_ERROR ) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    iResult = listen(serverSocket, SOMAXCONN);
+    if ( iResult == SOCKET_ERROR ) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    char buffer[DEFAULT_BUFLEN];
+    SOCKET clientSocket;
+
+    do {
+        clientSocket = accept(serverSocket, nullptr, nullptr);
+        if ( clientSocket == INVALID_SOCKET ) {
+            printf("accept failed with error: %d\n", WSAGetLastError());
+            closesocket(clientSocket);
+            continue;
+        }
+
+        do {
+            iResult = recv(clientSocket, buffer, DEFAULT_BUFLEN, 0);
+            if ( iResult > 0 )
+                {
+                    printf("Bytes received: %d\n\n", iResult);
+                    printf("received data:\n%s", buffer);
+                }
+            else if ( iResult == 0 )
+                {
+                    printf("Connection closed\n");
+                    closesocket(clientSocket);
+                }
+            else {
+                printf("recv failed: %d\n", WSAGetLastError());
+            }
+        } while ( iResult > 0 );
+
+    } while( 1 );
+    WSACleanup();
+    return 0;
+}
